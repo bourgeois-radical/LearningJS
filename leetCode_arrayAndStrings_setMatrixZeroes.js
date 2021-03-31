4e352f334dd24a34a7a48b2e2ab8267010759998
bourgeois-radical <78738464+bourgeois-radical@users.noreply.github.com>
2021-03-31 14:58:11 +0300

    done@

--- leetCode_arrayAndStrings_setMatrixZeroes.js
+++ leetCode_arrayAndStrings_setMatrixZeroes.js
@@ -4,7 +4,7 @@ Follow up:
 
 A straight forward solution using O(mn) space is probably a bad idea.
 A simple improvement uses O(m + n) space, but still not the best solution.
-Could you devise a constant space solution?*/
+Could you devise a constant space solution?!*/
 
 
 let matrix = [
