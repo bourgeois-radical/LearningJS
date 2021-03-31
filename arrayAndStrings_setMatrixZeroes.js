294a362027b4a70e9adb74105ecce8dee14c46ad
bourgeois-radical <78738464+bourgeois-radical@users.noreply.github.com>
2021-03-31 12:57:41 +0300

    some new code

--- arrayAndStrings_setMatrixZeroes.js
+++ arrayAndStrings_setMatrixZeroes.js
@@ -1,57 +1,37 @@
-/*let fruits = ["Яблочки", "Груши", "Жаботикаба", "Сметанное яблоко"];
-
-for (i = 0; i < fruits.length; i++) {
-	console.log(fruits[i]);
-}
-*/
-
-// Given an m x n matrix. If an element is 0, set its entire row and column to 0
-
 let matrix = [
-	[1, 1, 1, 1],
-	[1, 0, 1, 1],
-	[1, 1, 1, 1],
-	[1, 1, 1, 1]
-];
+	[1, 1, 0, 1, 1],
+	[1, 1, 1, 1, 1],
+	[1, 1, 1, 0, 1]
+]
 
-let rowsWhichSchouldContain0 = [];
-let columnsWhichSchouldContain0 = [];
+let i_0 = [];
+let j_0 = [];
+let u, j, k;
 
-//let newMatrix = [];
-
-let rw, clmn;
-
-//console.log(matrix[1][1]);
-//console.log(matrix.length)
-
-//here we are trying to find zeros in the given array
 for (let i = 0; i < matrix.length; i++) {
 	for (let j = 0; j < matrix.length; j++) {
-		//console.log(matrix[i][j]);
 		if (matrix[i][j] == 0) {
-			rw = i;
-			clmn = j;
-			rowsWhichSchouldContain0.push(rw);
-			columnsWhichSchouldContain0.push(clmn);
-		}
-	}
-	//console.log(matrixWithZeros.length);
-}
-
-//replacement 
-
-for (let q = 0; q < matrix.length; q++) {
-	for (let h = 0; h < matrix.length; h++) {
-		if (h == columnsWhichSchouldContain0[h]) {
-			matrix[q][h] = 0;
+			i_0.push(i);
+			j_0.push(j);
 		}
 	}
 }
 
-//output mewMatrix; 
-for (let k = 0; k < matrix.length; k++) {
-	for (let u = 0; u < matrix.length; u++) {
-		console.log(matrix[k][u]);
+u = 0;
+j = 0;
+k = i_0[u];
+
+while (k < i_0.length) {
+	while (j < matrix.length) {
+		matrix[k][j] = 0;
 	}
-	console.log(" ");
+	u++;
+	k = i_0[u];
+}
+
+for (let i = 0; i < matrix.length; i++) {
+	for (let j = 0; j < matrix.length; j++) {
+		console.log("новая матрица: ");
+		console.log(matrix[i][j]);
+	}
 }
