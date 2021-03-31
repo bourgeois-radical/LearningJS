16d086a6d15b78e734be5909769321f8b4dc0670
bourgeois-radical <78738464+bourgeois-radical@users.noreply.github.com>
2021-03-31 14:35:50 +0300

    done

--- arrayAndStrings_setMatrixZeroes.js
+++ arrayAndStrings_setMatrixZeroes.js
@@ -1,3 +1,12 @@
+/*Given an m x n matrix. If an element is 0, set its entire row and column to 0. Do it in-place.
+
+Follow up:
+
+A straight forward solution using O(mn) space is probably a bad idea.
+A simple improvement uses O(m + n) space, but still not the best solution.
+Could you devise a constant space solution?*/
+
+
 let matrix = [
 	[1, 1, 0, 1, 1],
 	[1, 1, 1, 1, 1],
@@ -8,8 +17,11 @@ let i_0 = [];
 let j_0 = [];
 let u, j, k;
 
-for (let i = 0; i < matrix.length; i++) {
-	for (let j = 0; j < matrix.length; j++) {
+const matrix_height = matrix.length;
+const matrix_length = matrix[0].length;
+
+for (let i = 0; i < matrix_height; i++) {
+	for (let j = 0; j < matrix_length; j++) {
 		if (matrix[i][j] == 0) {
 			i_0.push(i);
 			j_0.push(j);
@@ -17,21 +29,31 @@ for (let i = 0; i < matrix.length; i++) {
 	}
 }
 
+
 u = 0;
 j = 0;
 k = i_0[u];
 
-while (k < i_0.length) {
-	while (j < matrix.length) {
+while (u < i_0.length) {
+	while (j < matrix_length) {
 		matrix[k][j] = 0;
+		j++;
 	}
+	j = 0;
 	u++;
 	k = i_0[u];
 }
 
-for (let i = 0; i < matrix.length; i++) {
-	for (let j = 0; j < matrix.length; j++) {
-		console.log("новая матрица: ");
-		console.log(matrix[i][j]);
+u = 0;
+j = 0;
+k = j_0[u];
+
+while (u < j_0.length) {
+	while (j < matrix_height) {
+		matrix[j][k] = 0;
+		j++;
 	}
+	j = 0;
+	u++;
+	k = j_0[u];
 }
